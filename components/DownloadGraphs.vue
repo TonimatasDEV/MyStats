30abea90a7c72d08774758ae766bece304a08b27
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-05-28 22:13:18 +0200

    feat: Add SpigotMC, Hangar and more projects to Modrinth and CurseForge

--- components/DownloadGraphs.vue
+++ components/DownloadGraphs.vue
@@ -19,8 +19,19 @@ const projectsCurseForge = [
     name: 'KrystalCraft',
     url: 'https://www.modpackindex.com/api/v1/mod/31992',
     extract: data => data?.value?.data?.download_count
+  },
+  {
+    name: 'MythLib',
+    url: 'https://www.modpackindex.com/api/v1/mod/31992',
+    extract: data => data?.value?.data?.download_count
+  },
+  {
+    name: 'ForgeFPS',
+    url: 'https://www.modpackindex.com/api/v1/modpack/64705',
+    extract: data => data?.value?.data?.download_count
   }
 ]
+
 const projectsModrinth = [
   {
     name: 'Packet Fixer',
@@ -46,10 +57,53 @@ const projectsModrinth = [
     name: 'KrystalCraft',
     url: 'https://api.modrinth.com/v2/project/krystalcraft',
     extract: data => data?.value?.downloads
+  },
+  {
+    name: 'MythLib',
+    url: 'https://api.modrinth.com/v2/project/mythlib',
+    extract: data => data?.value?.downloads
+  },
+  {
+    name: 'PerWorldCommands',
+    url: 'https://api.modrinth.com/v2/project/perworldcommands',
+    extract: data => data?.value?.downloads
+  },
+  {
+    name: 'InstaKillMobs',
+    url: 'https://api.modrinth.com/v2/project/instakillmobs',
+    extract: data => data?.value?.downloads
+  }
+]
+
+const projectsSpigot = [
+  {
+    name: 'PerWorldPlugins',
+    url: 'https://api.spiget.org/v2/resources/96161',
+    extract: data => data?.value?.downloads
+  },
+  {
+    name: 'PerWorldCommands',
+    url: 'https://api.spiget.org/v2/resources/97003',
+    extract: data => data?.value?.downloads
+  },
+  {
+    name: 'InstaKillMobs',
+    url: 'https://api.spiget.org/v2/resources/96185',
+    extract: data => data?.value?.downloads
+  }
+]
+
+const projectsHangar = [
+  {
+    name: 'PerWorldPlugins',
+    url: 'https://hangar.papermc.io/api/v1/projects/PerWorldPlugins',
+    extract: data => data?.value?.stats?.downloads
   }
 ]
 </script>
 <template>
   <BarGraph title="CurseForge downloads" :projects="projectsCurseForge"/>
   <BarGraph title="Modrinth downloads" :projects="projectsModrinth"/>
+  <BarGraph title="SpigotMC downloads" :projects="projectsSpigot"/>
+  <BarGraph title="Hangar downloads" :projects="projectsHangar"/>
 </template>
