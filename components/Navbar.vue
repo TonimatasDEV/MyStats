d80688e9d7ef19a8617f68612cecf527c88d0d51
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-08-16 16:46:03 +0200

    chore: Change name and home url

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,10 +2,10 @@
   <nav class="">
     <div class="bg-black max-w-7xl mx-auto max-h-fit px-10 py-4 flex items-center justify-between rounded-b-2xl">
       <div class="text-xl font-bold">
-        <NuxtLink to="/">My project stats</NuxtLink>
+        <NuxtLink to="/">Stats by Project</NuxtLink>
       </div>
       <ul class="flex space-x-8">
-        <li><NuxtLink to="/" class="hover:text-gray-400">Home</NuxtLink></li>
+        <li><NuxtLink to="https://tonimatas.dev" class="hover:text-gray-400">Home</NuxtLink></li>
         <li>
           <NuxtLink to="https://github.com/TonimatasDEV/MyStats" target="_blank" external class="hover:text-gray-400">
             <Icon name="uil:github" style="color: white" size="25px"/>
