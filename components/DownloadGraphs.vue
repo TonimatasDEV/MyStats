e2d5b730657780141a122b2bcfdad83e60bc017b
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2026-02-04 19:39:12 +0100

    chore: Remove unnecessary component

--- components/DownloadGraphs.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <BarGraph title="CurseForge downloads" api="curseforge"/>
-  <BarGraph title="Modrinth downloads" api="modrinth"/>
-  <BarGraph title="SpigotMC downloads" api="spiget"/>
-  <BarGraph title="Hangar downloads" api="hangar"/>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,11 @@
       <ModpacksWithGraph name="Packet Fixer" id="39585"/>
       <ModpacksWithGraph name="Mekanism Curios" id="77177"/>
     </div>
-    <DownloadGraphs/>
+
+    <BarGraph title="CurseForge downloads" api="curseforge"/>
+    <BarGraph title="Modrinth downloads" api="modrinth"/>
+    <BarGraph title="SpigotMC downloads" api="spiget"/>
+    <BarGraph title="Hangar downloads" api="hangar"/>
   </div>
 </template>
 <script setup lang="ts">
